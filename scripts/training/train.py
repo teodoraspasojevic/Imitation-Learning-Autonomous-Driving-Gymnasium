8f22f0ccf0a485281c59f766b9838ab7243ac8b8
teodoraspasojevic <spasojevic.teodora@gmail.com>
2024-05-28 12:25:21 +0200

    Added data and model loading

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -1,8 +1,14 @@
-from data import CarDataset
+from data import CarDataset, RandomHorizontalFlipWithLabel, RandomVerticalFlipWithLabel, ChangeStreetColor, ToTensorWithLabel, ComposeTransformations
+from model import Model, to_device, train_model, test_model, plot_history
 from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
+import torch.optim as optim
 
-car_dataset = CarDataset(root='../../data', transform=None)
-car_dataset.visualize()
+transforms = ComposeTransformations([
+    ToTensorWithLabel()
+])
+
+car_dataset = CarDataset(root='../../data', transform=transforms)
 
 train_size = int(0.6 * len(car_dataset))
 val_train_size = len(car_dataset) - train_size
@@ -20,3 +26,9 @@ train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=False)
 val_loader = DataLoader(dataset=val_dataset, batch_size=64, shuffle=False)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
+model = Model()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+train_model(model, train_loader, val_loader, criterion, optimizer, epochs=10)
