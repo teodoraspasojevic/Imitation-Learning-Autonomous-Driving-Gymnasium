435e243661a365e2614c59f8c627962f736f7088
teodoraspasojevic <spasojevic.teodora@gmail.com>
2024-05-28 12:29:20 +0200

    Changed the number of input channels so we can process RGB images

--- scripts/training/model.py
+++ scripts/training/model.py
@@ -29,7 +29,7 @@ class EarlyStopping:
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
