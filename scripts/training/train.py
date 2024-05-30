99aea7c8d459272772d63c28e6a239db6a05b032
teodoraspasojevic <spasojevic.teodora@gmail.com>
2024-05-30 12:05:34 +0200

    Manually split data, so to avoid shuffle

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -1,29 +1,30 @@
-from data import CarDataset, RandomHorizontalFlipWithLabel, RandomVerticalFlipWithLabel, ChangeStreetColor, ToTensorWithLabel, ComposeTransformations
+from data import CarDataset, ResizeWithLabels, RandomHorizontalFlipWithLabel, RandomVerticalFlipWithLabel, ChangeStreetColor, ToTensorWithLabel, ComposeTransformations
 from model import Model, to_device, train_model, test_model, plot_history
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset
 import torch.nn as nn
 import torch.optim as optim
 
 transforms = ComposeTransformations([
+    ResizeWithLabels(),
     ToTensorWithLabel()
 ])
 
 car_dataset = CarDataset(root='../../data', transform=transforms)
 
 train_size = int(0.6 * len(car_dataset))
-val_train_size = len(car_dataset) - train_size
+val_size = int(0.2 * len(car_dataset))
+test_size = len(car_dataset) - train_size - val_size
 
-train_dataset, val_test_dataset = random_split(dataset=car_dataset, lengths=[train_size, val_train_size])
+train_indices = list(range(train_size))
+val_indices = list(range(train_size, train_size + val_size))
+test_indices = list(range(train_size + val_size, len(car_dataset)))
 
-val_size = int(0.5 * len(val_test_dataset))
-test_size = len(val_test_dataset) - val_size
+train_dataset = Subset(car_dataset, train_indices)
+val_dataset = Subset(car_dataset, val_indices)
+test_dataset = Subset(car_dataset, test_indices)
 
-val_dataset, test_dataset = random_split(dataset=val_test_dataset, lengths=[val_size, test_size])
-
-# We do not shuffle any dataset because we need frames to be consecutive.
-
-train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=False)
-val_loader = DataLoader(dataset=val_dataset, batch_size=64, shuffle=False)
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 model = Model()
@@ -31,4 +32,9 @@ model = Model()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-train_model(model, train_loader, val_loader, criterion, optimizer, epochs=10)
+model, history, best_epoch = train_model(model, train_loader, val_loader, criterion, optimizer, epochs=10)
+
+plot_history(history, best_epoch)
+
+test_model(model, test_loader)
+
